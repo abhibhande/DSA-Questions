2e8a495d3c90ddcc0d2a9780a792b7d00ed4d8d3
unknown <abhishekbhande248@gmail.com>
2024-06-06 16:15:22 +0530

    Another Day of DSA Coding With the Striver A to Z sheet

---
+++ Majority Element II/Optimal_approach.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        unordered_map<int,int> m;
+        vector<int> ans;
+        for(auto i:nums)
+        {
+            m[i]++;
+        }
+
+        for(auto i:m)
+        {
+            if(i.second>nums.size()/3)
+            {
+                ans.push_back(i.first);
+            }
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
