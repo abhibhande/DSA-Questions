7f3f43f04d2926687a4b9852c0e2d84f75affab0
unknown <abhishekbhande248@gmail.com>
2024-06-27 21:32:14 +0530

    I forget to upload the POTD. It's an easy One

---
+++ 0 POTD Solution/1791. Find Center of Star Graph/Solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        int ans=0;
+        if(edges[0][0]==edges[1][0] || edges[0][0]==edges[1][1])
+        {
+            ans=edges[0][0];
+        }
+        else
+        {
+            ans=edges[0][1];
+        }
+        return ans;
+    }
+};
